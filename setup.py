e43e35a5a3705ce411a60d82fea1a9539a8a9d60
eduardosprp <47829981+eduardosprp@users.noreply.github.com>
2022-06-13 01:29:07 -0300

    Finally managed to upload to pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyCRT",
-    version="2.0.0",
+    version="2.0.2",
     author="Eduardo Lopes Dias",
     author_email="eduardosprp@protonmail.com",
     description="Tools for measuring Capillary Refill Time using Python.",
